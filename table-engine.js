96adde1c78751420485ae78e7c079bfc1ec2370e
jt1919191919 <124062484+jt1919191919@users.noreply.github.com>
2026-04-09 13:43:48 -0400

    Update table-engine.js
    fixing tables sticky scrolling

--- table-engine.js
+++ table-engine.js
@@ -201,7 +201,7 @@ function renderTable(container, tableId, rows, visibleCols, sortCol, sortDir, is
   </div>`;
 
   // Table
-  html += `<div class="spell-table-scroll"><table class="spell-table" id="spell-table-${tableId}">
+  html += `<div class="spell-table-scroll" style="overflow:auto;max-height:600px"><table class="spell-table" id="spell-table-${tableId}">
     <thead><tr>`;
   cols.forEach(c => {
     const sortKey = c.sortKey || c.key;
